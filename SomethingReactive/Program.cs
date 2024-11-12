777f4c02e678271ff7270b835731090d93439277
Luis Harvey Triana Vega <harveytriana@hotmail.com>
2024-11-11 22:54:00 -0500

    Labels

--- SomethingReactive/Program.cs
+++ SomethingReactive/Program.cs
@@ -3,8 +3,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SomethingReactive;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.RootComponents.Add<App>("#app");
 
 await builder.Build().RunAsync();
